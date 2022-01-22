f299bb67177f5625a116e158d5fcabe666aea05f
gordon0813 <32182281+gordon0813@users.noreply.github.com>
2022-01-22 21:23:09 +0800

    readme

--- script_test.sh
+++ script_test.sh
@@ -1,4 +1,4 @@
-read /Users/gordon/Documents/GitHub/benchmarks-master/random_control/router.v
+read /Users/gordon/Documents/GitHub/benchmarks-master/random_control/voter.v
 strash
 lsv_sat_NodeSweep    10 30
 #/Users/gordon/Documents/GitHub/benchmarks-master/random_control/ctrl.v
@@ -6,12 +6,13 @@ lsv_sat_NodeSweep    10 30
 #/Users/gordon/Documents/GitHub/benchmarks-master/random_control/router.v
 #0 30 308  :  0 20  224     :10  30  132   : 30 30 133  time used per iteration:21ms
 #/Users/gordon/Documents/GitHub/benchmarks-master/random_control/priority.v
-# 0 30 901 :  0:20 560 : 20 30 1069 :  30 30 1069  :-1  30 855
+# 0 30 901 :  0:20 560 : 20 30 1069 :  30 30 1069  :-1  30 855 time used per iteration:991ms
 #/Users/gordon/Documents/GitHub/benchmarks-master/arithmetic/adder.v
-# 0 30 1279  : 0 20 1272  :   20 30  1154   : 30  30 1155  
+# 0 30 1279  : 0 20 1272  :   20 30  1154   : 30  30 1155   time used per iteration:339ms
 #/Users/gordon/Documents/GitHub/benchmarks-master/random_control/i2c.v
-# 0 30 1425  :  0 20 1328 :    20 30 1441   : 30 30  1446 :-1  30 1403 used:50241044
+# 0 30 1425  :  0 20 1328 :    20 30 1441   : 30 30  1446 :-1  30 1403 time used per iteration:1832ms
 #/Users/gordon/Documents/GitHub/benchmarks-master/random_control/cavlc.v
-# 0 30 669   : 0 20   611 :    20 30  480   : 30 30  485  :-1  30 405  time used:3884815
+# 0 30 669   : 0 20   611 :    20 30  498   : 30 30  485  :-1  30 405  time used per iteration:117ms
 #/Users/gordon/Documents/GitHub/benchmarks-master/random_control/int2float.v
-# 0 30 256   : 0 20   197 :    20 30  203   : 30 30  208   used:1036068
+# 0 30 256   : 0 20   197 :    20 30  203   : 30 30  208   time used per iteration:29ms
+#/Users/gordon/Documents/GitHub/benchmarks-master/random_control/voter.v
