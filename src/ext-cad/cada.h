f79ea8a9657db3a17bb44a964628d230dfe4a9c0
gordon0813 <32182281+gordon0813@users.noreply.github.com>
2022-07-10 15:58:44 +0800

    cad a

--- src/ext-cad/cada.h
+++ src/ext-cad/cada.h
@@ -6,10 +6,12 @@
 #include <vector>
 #include <fstream>
 #include <sstream>
+#include <bitset>
 
 #define ANDTYPE 0
 #define XORTYPE 1
 #define MUXTYPE 2
+using namespace std;
 void Lsv_NtkPrintNodes(Abc_Ntk_t*  pNtk) ;
 void CadA(Abc_Ntk_t*  pNtk ,std::string ofile);
 
@@ -27,24 +29,37 @@ class Component{
     std::vector<Gia_Obj_t  *>outNodes;
     int mark;
 };
+
 class CompXor :public Component{
     virtual int thetype() override{return XORTYPE;} 
     virtual std::string thecolor()override{return "red";} 
 };
 
+
 class NodeInfo{
     public:
-    NodeInfo(int id){nodeId=id;}
+    NodeInfo(int id){nodeId=id;fix=0;}
     int nodeid(){return nodeId;}
     int addMatchComponent(Component* c){matchComps.push_back(c);}
     std::string ComponentColor();
+    int setSim(int v){if(fix==0)simValue=v; return fix;};
+    int lock(){fix=1;}
+    int unlock(){fix=0;}
+    int savesim(){simvalueSave=simValue;}
+    int xorChainCandidate(){return (simValue^simvalueSave)==~0; }
+    int simValue;
     private:
+    int simvalueSave;
+    short fix;
+
     int nodeId;
+    
     std::vector<Component*> matchComps;
 };
 class Collection{
     public:
-    Collection(Gia_Man_t * p){
+    Collection(Gia_Man_t * pp){
+        Gia_Man_t *p=Gia_ManDupOrderDfs(pp);
         int totalnum=Gia_ManObjNum(p);
         this->giacir=p;
         int i;
@@ -58,30 +73,90 @@ class Collection{
         }
     }
     void showInfo(std::string filename);
-    int  detectXor(){
-        int i,id;
-        int count=0;
-        Gia_Obj_t  *pobj, * pFan0, * pFan1; 
-        pobj=getobj(id);
-        Gia_ManForEachObj(giacir,pobj,i){
-            if(Gia_ObjRecognizeExor(pobj, &pFan0, &pFan1)){
-                std::cout<<"xor"<<id<<"\n";
-                Component * xgate=new CompXor();
-                xgate->push_input(pFan0);
-                xgate->push_input(pFan1);
-                xgate->push_output(pobj);
-                setInfo(pFan0,xgate);
-                setInfo(pFan1,xgate);
-                setInfo(pobj,xgate);
-                count++;
-            }
-        } 
-        return count ;
+    int  detectXor();
+    int randomSim();
+   
+    int xorChain(Gia_Obj_t * psa){
+    Gia_Obj_t * pobj;
+    int i;
+    int sa01=0;
+    //randon input
+    NodeInfo * san=getInfo(psa);
+    san->setSim(sa01);
+    san->lock();
+    Gia_ManForEachCi(giacir,pobj,i){
+        NodeInfo* ci= this->getInfo(pobj);
+       // cout <<Gia_ObjId(giacir,pobj)<<endl;
+        ci->setSim(rand());
     }
+    Gia_ManForEachObj(giacir,pobj,i){
+        if(Gia_ObjIsAnd(pobj)){
+            simNode(pobj);
+        }
+    }
+    int a=0;
+        
+    /**  
+    Gia_ManForEachObj(giacir,pobj,i){
+        a=getInfo(pobj)->simValue;
+        bitset<32> x(a);
+        cout <<Gia_ObjId(giacir,pobj)<<" "<< x <<endl;
+
+    }**/
+    san->unlock();
+    //save sa0 value
+    Gia_ManForEachObj(giacir,pobj,i){
+        getInfo(pobj)->savesim();
+    }
+    sa01=~0;
+    san->setSim(sa01);
+    san->lock();
+    Gia_ManForEachObj(giacir,pobj,i){
+        if(Gia_ObjIsAnd(pobj)){
+            simNode(pobj);
+        }
+    }
+    /**
+    Gia_ManForEachObj(giacir,pobj,i){
+        a=getInfo(pobj)->simValue;
+        bitset<32> x(a);
+        cout <<Gia_ObjId(giacir,pobj)<<" "<< x <<endl;
+
+    }**/
+    san->unlock();
+    cout<<Gia_ObjId(giacir,psa)<<" : ";
+    Gia_ManForEachObj(giacir,pobj,i){
+        if(getInfo(pobj)->xorChainCandidate())cout<<Gia_ObjId(giacir,pobj)<<" ";
+        //cout <<Gia_ObjId(giacir,pobj)<<" "<< getInfo(pobj)->xorChainCandidate()<<endl;
+    }
+    cout<<endl;
+    }
+    int xorChainAll(){
+        Gia_Obj_t * pobj;
+        int i;
+        Gia_ManForEachObj(giacir,pobj,i){
+            xorChain(pobj);
+        }
+    }
+
+
+
     private:
     Gia_Obj_t  * getobj(int id){return Gia_ManObj(giacir,id);}
     int setInfo(Gia_Obj_t  *n,Component * matchcomp){
        ninfos[ Gia_ObjId(giacir,n) ]->addMatchComponent(matchcomp);
+    }
+    NodeInfo* giaInfo(Gia_Obj_t  *n){return ninfos[Gia_ObjId(giacir,n)];}
+    int simNode(Gia_Obj_t  * thenode){
+       // Gia_Obj_t  * thenode=getobj(id);
+        NodeInfo* info=giaInfo(thenode);
+        NodeInfo* linfo=giaInfo(Gia_ObjFanin0(thenode));
+        int linv=Gia_ObjFaninC0(thenode);
+        NodeInfo* rinfo=giaInfo(Gia_ObjFanin1(thenode));
+        int rinv=Gia_ObjFaninC1(thenode);
+       // cout<<info->nodeid()<<" "<<linv<<" "<<rinv<<endl;
+        int v= ((linv==0)?(linfo->simValue):(~linfo->simValue) )& ((rinv==0)?(rinfo->simValue):(~rinfo->simValue));
+        info->setSim(v);
     }
     NodeInfo* getInfo(Gia_Obj_t  *n){return ninfos[ Gia_ObjId(giacir,n) ]; }
     std::string edgeFeature(Gia_Obj_t  *n,int fi){
--- src/ext-cad/mapping.cpp
+++ src/ext-cad/mapping.cpp
@@ -1,6 +1,30 @@
 #include "ext-cad/cada.h"
 
+using namespace std;
+int  Collection::detectXor(){
+        int i,id;
+        int count=0;
+        Gia_Obj_t  *pobj, * pFan0, * pFan1; 
+        pobj=getobj(id);
+        Gia_ManForEachObj(giacir,pobj,i){
+            if(Gia_ObjRecognizeExor(pobj, &pFan0, &pFan1)){
+                std::cout<<"xor"<<id<<"\n";
+                Component * xgate=new CompXor();
+                xgate->push_input(pFan0);
+                xgate->push_input(pFan1);
+                xgate->push_output(pobj);
+                setInfo(pFan0,xgate);
+                setInfo(pFan1,xgate);
+                setInfo(pobj,xgate);
+                count++;
+            }
+        } 
+        return count ;
+    }
 void Collection::showInfo(std::string filename){
+
+
+        xorChainAll();
         Gia_Obj_t * pobj;
         int i;
         std::ofstream myfile;
@@ -11,12 +35,40 @@ void Collection::showInfo(std::string filename){
                 myfile<<" a"<<Gia_ObjId(giacir,Gia_ObjFanin0(pobj))<<" -> "<<" a"<<Gia_ObjId(giacir,pobj)<<edgeFeature(pobj,Gia_ObjFaninC0(pobj))<<";\n";
             }else if(Gia_ObjIsAnd(pobj)){
                 myfile<<" a"<<Gia_ObjId(giacir,Gia_ObjFanin0(pobj))<<" -> "<<" a"<<Gia_ObjId(giacir,pobj)<<edgeFeature(pobj,Gia_ObjFaninC0(pobj))<<";\n";
-                myfile<<" a"<<Gia_ObjId(giacir,Gia_ObjFanin1(pobj))<<" -> "<<" a"<<Gia_ObjId(giacir,pobj)<<edgeFeature(pobj,Gia_ObjFaninC0(pobj))<<";\n";
+                myfile<<" a"<<Gia_ObjId(giacir,Gia_ObjFanin1(pobj))<<" -> "<<" a"<<Gia_ObjId(giacir,pobj)<<edgeFeature(pobj,Gia_ObjFaninC1(pobj))<<";\n";
             }
         }
         myfile<<"}\n";
         myfile.close();
     }
+    /**
+int  Collection::xorChain(){
+    Gia_Obj_t * pobj;
+    int i;
+    //randon input
+    Gia_ManForEachCi(giacir,pobj,i){
+        NodeInfo* ci= this->getInfo(pobj);
+        ci->setSim(rand());
+    }
+    Gia_ManForEachObj(giacir,pobj,i){
+        if(Gia_ObjIsAnd(pobj)){
+            simNode(pobj);
+        }
+    }
+    int a=0;
+        
+        
+    Gia_ManForEachObj(giacir,pobj,i){
+        a=getInfo(pobj)->simValue;
+        bitset<32> x(a);
+        cout <<Gia_ObjId(giacir,pobj)<<" "<< x <<endl;
+
+    }
+    
+
+
+
+}**/
  std::string   NodeInfo::ComponentColor(){
      int match_count=0;
      std::stringstream ss;
